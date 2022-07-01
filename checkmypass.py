1aa8a8a2a6b8ca4bb0c0e89b0d6ae0d54f65da64
KeanoW <winterkeano02@gmail.com>
2022-07-01 15:52:59 +0200

    Connecting to Api working on checking password through api

--- checkmypass.py
+++ checkmypass.py
@@ -1,5 +1,6 @@
 import requests
 import hashlib
+import sys
 
 def request_api_data(query_char):
     url = "https://api.pwnedpasswords.com/range/" + query_char
@@ -8,7 +9,24 @@ def request_api_data(query_char):
         raise RuntimeError(f"Erorr fetching: {res.status_code}", "check api and try again.")
     return res
 
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(":") for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+
 def password_api_check(password):
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)
+    return get_password_leaks_count(response, tail)
 
+def main(args):
+    for password in args:
+        count = password_api_check(password)
+        if count:
+            print(f"{password} was found {count} times.. you should change your password!")
+        else:
+            print(f"{password} was NOT found.. Carry on!")
 
-# request_api_data("password123")
+main(sys.argv[1:])
